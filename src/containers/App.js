fdfbc3b8943cc1a423ed2acca16a419c5cb163cd
vrilli <vrillyjcr@gmail>
2022-01-25 23:05:38 -0500

    hola

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,15 @@
+import Counter from "../components/Counter";
 import Guajolote from "../components/Guajolote";
 
 function App() {
   return (
     <div className="App">
       <Guajolote/> 
+      <Counter/>
     </div>
   );
 }
 
 export default App;
+
+
