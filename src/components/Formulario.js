40e1e6d862f4397c9b8d9fbfe96c28a5a0474dcb
vrilli <vrillyjcr@gmail>
2022-01-26 13:09:37 -0500

    hi

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+
+
 
 const Formulario = () => {
   return (
-    <div>
-  
-    </div>);
+     <Fragment>
+        <h2 className='mt-5'>Formulario de Registro</h2>
+        <form className='row'>
+          <div className='col-md-6'>
+          <input className='form-control' type="name" placeholder='Nombre Completo' />
+          </div>
+          <div className='col-md-6'>
+          <input className='form-control' type="email" placeholder='Correo Electronico' />
+          </div>
+          <div className='col-md-6'>
+          <input className='form-control' type="password" placeholder='Contraseña' />
+          </div>
+          <div className='col-md-6'>
+          <input className='form-control' type="password" placeholder='Repita su Contraseña' />
+          </div>
+
+        
+        </form>
+    </Fragment>
+    
+    
+    );
 };
 
 export default Formulario;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 
+import Formulario from "../components/Formulario";
 import Guajolote from "../components/Guajolote";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App mt-5">
       <Guajolote/> 
-      
+      <Formulario/>
     </div>
   );
 }
